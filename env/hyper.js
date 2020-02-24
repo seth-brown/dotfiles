d2dd23c18f64bbbbc11f687d4b98b5ca37035938
Seth Brown <software@seth-brown.net>
2020-02-24 08:27:00 -0500

    unified monokai aesthetic across system

--- env/hyper.js
+++ env/hyper.js
@@ -131,7 +131,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-materialshell"],
+  plugins: ["hyper-materialshell", "hyper-monokai-deluxe"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
