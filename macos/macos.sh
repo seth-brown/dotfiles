75b3254ea13da45ba7441000193ae7b237ff972d
Seth Brown <software@seth-brown.net>
2020-02-24 07:25:11 -0500

    fixed erroneous key repeats

--- macos/macos.sh
+++ macos/macos.sh
@@ -1,6 +1,6 @@
 # Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool true
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
