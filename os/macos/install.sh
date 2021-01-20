006e126675aa18a0a77e2d4ca216727ac2a1a993
Seth Brown <software@seth-brown.net>
2021-01-20 14:31:52 -0500

    .

--- os/macos/install.sh
+++ os/macos/install.sh
@@ -29,22 +29,21 @@ PACKAGES="zsh
           python \
           tmux \
           cmake \
-          reattach-to-user-namespace"
-          # git
+          reattach-to-user-namespace \
+          git"
 
 # install homebrew and deps
 CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# brew update || brew update
 brew update || brew upgrade
 brew install $PACKAGES
-# brew cask install hyper amethyst
-# brew tap homebrew/cask-fonts
-# brew cask install font-fira-code
+brew cask install hyper amethyst
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
 
-# $SHELL $CUR_DIR/os/base.sh
-# $SHELL $CUR_DIR/os/symlinks.sh
-# $SHELL $CUR_DIR/os/python.sh
-# $SHELL $CUR_DIR/os/node.sh
+$SHELL $CUR_DIR/os/base.sh
+$SHELL $CUR_DIR/os/symlinks.sh
+$SHELL $CUR_DIR/os/python.sh
+$SHELL $CUR_DIR/os/node.sh
 
-# $SHELL $CUR_DIR/os/vim.sh
-# $SHELL $CUR_DIR/os/macos/duti.sh
+$SHELL $CUR_DIR/os/vim.sh
+$SHELL $CUR_DIR/os/macos/duti.sh
