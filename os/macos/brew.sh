d3a659f1214008a15b6dd08b403292765a08d916
Seth Brown <software@seth-brown.net>
2021-11-29 06:49:49 -0500

    refactor: updated configs for MacOS Monterey
    * fzf: updated old install cmd syntax
    * homebrew: fixed install issues with sudo, migrated logic to seperate
      config (os/mac/brew.sh), updated package list

---
+++ os/macos/brew.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# terminate when an error occurs
+set -o errexit
+set -o pipefail
+
+# detect uninitialised vars and terminate
+set -o nounset
+
+# prints every expression before executing it
+set -o xtrace
+
+# needed by homebrew for some package executables
+export PATH="/usr/local/sbin:$PATH"
+PACKAGES=(zsh 
+          jq
+          bat
+          exa
+          duti
+          fzf
+          git
+          readline
+	  zoxide
+          xz
+          diff-so-fancy
+          ripgrep
+          neofetch
+          neovim
+          python
+          tmux 
+          cmake
+	  reattach-to-user-namespace)
+
+# install homebrew and deps
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# add brew to path
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/sbrown/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew update
+brew upgrade
+brew install ${PACKAGES[@]}
+
+$(brew --prefix)/opt/fzf/install --all \
+
+# brew cask install hyper amethyst 
+brew install --cask kitty amethyst
--- os/macos/install.sh
+++ os/macos/install.sh
@@ -12,37 +12,7 @@ set -o xtrace
 
 CUR_DIR=$(pwd -P)
 
-# needed by homebrew for some package executables
-export PATH="/usr/local/sbin:$PATH"
-PACKAGES="zsh \
-          jq \
-          bat \
-          exa \
-          duti \
-          fzf \
-          git \
-          readline \
-          xz \
-          diff-so-fancy \
-          ripgrep \
-          neofetch \
-          neovim \
-          python \
-          tmux \
-          cmake \
-          reattach-to-user-namespace"
-
-# install homebrew and deps
-CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew update
-brew upgrade
-brew install $PACKAGES
-brew tap homebrew/cask-fonts
-
-# brew cask install hyper amethyst 
-brew install --cask kitty amethyst
-brew install font-fira-code
-
+$SHELL $CUR_DIR/os/macos/brew.sh
 $SHELL $CUR_DIR/os/base.sh
 $SHELL $CUR_DIR/os/symlinks.sh
 $SHELL $CUR_DIR/os/python.sh
@@ -50,3 +20,5 @@ $SHELL $CUR_DIR/os/node.sh
 
 $SHELL $CUR_DIR/os/vim.sh
 $SHELL $CUR_DIR/os/macos/duti.sh
+
+
