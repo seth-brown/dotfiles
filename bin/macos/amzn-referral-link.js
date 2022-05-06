6b8713d2edc865bca999987102ad2e79c9eb9a09
Seth Brown <software@seth-brown.net>
2022-05-06 05:35:00 -0400

    refactor: updated JS scripts

--- bin/macos/amzn-referral-link.js
+++ bin/macos/amzn-referral-link.js
@@ -1,10 +1,10 @@
 // to shorten a valid Amazon link in the front most tab:
 // $ osascript amzn-referral-link.js
 
-var amznShortUrl = function (inputUrl) {
-  var baseUrl = "http://www.amzn.com",
-    trackingId = "drbu-20",
-    productIdMatch = inputUrl.match(
+const amznShortUrl = (inputUrl) => {
+  const baseUrl = "http://www.amzn.com"
+  const trackingId = "drbu-20"
+  const productIdMatch = inputUrl.match(
       "(amazon|amzn).*?(/[a-zA-Z0-9]{10})(?:[/?]|$)"
     );
 
